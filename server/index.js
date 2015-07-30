e6ef5102c8cb41a6a0142921d91e3608c8eb18bc
Vanessa Virgitti <virgitti@gmail.com>
2015-07-30 18:25:51 +0100

    changed server file but not passing test

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-  res.send("Lineup");
+  res.sendFile('index');
 });
 
 var port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log("App running on: " + port); 
+console.log("App running on: " + port);
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,11 +1,11 @@
 var app = require('../server/index.js');
+var should = require('should');
 var request = require('supertest');
 
 describe('GET /', function(){
   it('/ should respond with 200 and "Lineup"', function(done){
-    request.get('/')
-    .expect(200)
-    .expect("Lineup")
-    .end(done);
+    request(app)
+    .get('/')
+    .expect(200, done);
   });
 });
