5cc63f7007bf1efc8577e1bd46bf57983227fae1
Vanessa Virgitti <virgitti@gmail.com>
2015-07-31 14:22:12 +0100

    Fixed problem with Mocha and test passing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var express = require('express');
-var app = express();
 var path = require('path');
+
+var app = module.exports = express();
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
@@ -8,7 +11,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-  res.sendFile('index');
+  res.sendFile('index.html', {root: __dirname + '/views'});
 });
 
 var port = process.env.PORT || 3000;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,11 +1,12 @@
-var app = require('../server/index.js');
-var should = require('should');
-var request = require('supertest');
+'use strict';
 
-describe('GET /', function(){
-  it('/ should respond with 200 and "Lineup"', function(done){
-    request(app)
-    .get('/')
-    .expect(200, done);
+var app = require ('../index.js');
+var request = require('supertest').agent(app.listen());
+
+describe('GET /', function() {
+  it('/index.html should respond with 200', function (done) {
+    request.get('/index.html')
+    .expect(200)
+    .end(done);
   });
 });
