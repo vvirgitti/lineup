67974dad301a1652814ba88e4235b04861c2f60d
Vanessa Virgitti <virgitti@gmail.com>
2015-08-13 16:56:52 +0100

    Added a class and a timeout that is not working

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,6 +2,8 @@
 
 var app = require ('../index.js');
 var request = require('supertest').agent(app.listen());
+var chai = require('chai');
+var expect = chai.expect;
 
 describe('GET /', function() {
   it('/index.html should respond with 200', function (done) {
@@ -9,5 +11,11 @@ describe('GET /', function() {
     .expect(200)
     .end(done);
   });
+
+describe('Add players to the roster page', function(){
+  it('can add 2 players to the page', function(){
+    player.addPlayer
+  });
+});
 
 });
