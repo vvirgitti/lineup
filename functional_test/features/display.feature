6f84995b46958c5965ab1755a7fc3c03c36d12a8
Vanessa <virgitti@gmail.com>
2015-08-22 12:36:04 +0100

    Changed the functional test but need to find a way to be able to test 2 players at the same time

--- functional_test/features/display.feature
+++ functional_test/features/display.feature
@@ -1,15 +1,14 @@
-Feature: Display the interface to add new players
+Feature: Adding players to the roster
 
   Scenario: Display text boxes to add Names and Positions
     Given I am on the homepage
     Then I should see a box to enter a name
 
-  Scenario: Adding a new player
+  Scenario Outline: Adding several players
     Given I am on the homepage
-    When I add a name and position
-    Then I should see the name of the player and the position displayed
-
-  Scenario: Adding 2 players
-    Given I am on the homepage
-    When I add 2 names and 2 positions
-    Then I should see a list of names and positions
+    When I add "<Name>" and "<Position>"
+    Then I should see "<Name>" and "<Position>"
+    Examples:
+    | Name  | Position |
+    | Oscar | Catch    |
+    | Nathan| Pitcher  |
--- functional_test/features/step_definitions/display.rb
+++ functional_test/features/step_definitions/display.rb
@@ -6,27 +6,14 @@ Then(/^I should see a box to enter a name$/) do
   expect(page).to have_css('input[type="text"]')
 end
 
-When(/^I add a name and position$/) do
-  fill_in('playerName', :with => 'John')
-  fill_in('position', :with => 'Catch')
+When(/^I add "([^"]*)" and "([^"]*)"$/) do |name, position|
+  fill_in('playerName', :with => name)
+  fill_in('position', :with => position)
   click_button('Add Player')
+  sleep 3
 end
 
-Then(/^I should see the name of the player and the position displayed$/) do
-  expect(page).to have_content("John Catch")
-end
-
-When(/^I add (\d+) names and (\d+) positions$/) do |arg1, arg2|
-  fill_in('playerName', :with => 'John')
-  fill_in('position', :with => 'Catch')
-  click_button('Add Player')
-  click_link('Add more players')
-  fill_in('playerName', :with => 'Tim')
-  fill_in('position', :with => 'Pitch')
-  click_button('Add Player')
-end
-
-
-Then(/^I should see a list of names and positions$/) do
-  expect(page).to have_content("John Tim Catch Pitch")
+Then(/^I should see "([^"]*)" and "([^"]*)"$/) do |name, position|
+    expect(page).to have_content(name)
+    expect(page).to have_content(position)
 end
