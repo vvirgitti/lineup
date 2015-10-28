c2d96f413be6c479824fb963588486504c782873
Vanessa Virgitti <virgitti@gmail.com>
2015-10-28 13:44:02 +0000

    The index file now uses the right layout page but renders the header twice - needs to be fixed

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ var hbx = require('express-handlebars');
 
 var app = module.exports = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
-app.engine('handlebars', hbx({defaultLayout: 'layout'}));
-app.use(express.static(__dirname + '/views'));
-
-app.get('/', function(req, res){
-  res.sendFile('layout.hbs', {root: __dirname + '/views'});
+var hbs = hbx.create({
+  defaultLayout: 'layout',
+  extname: '.hbs',
+  layoutsDir: __dirname + '/views'
 });
 
-app.post('/roster', function(req, res){
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+
+app.use('/layout/src', express.static([__dirname, '/src'].join('')));
+app.use(express.static(__dirname + '/views'));
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  res.render('layout.hbs', {root: __dirname + '/views'});
+});
+
+// app.post('/roster', function(req, res){
   // var playerName  = req.body.playerName;
   // var position = req.body.position;
   // console.log('Player: ' + playerName + ' & position: ' + position);
   // res.redirect('http://localhost:3000/roster.html');
-});
+// });
 
 // app.get('/roster', function(req, res){
 //   res.send(playerName, position)
