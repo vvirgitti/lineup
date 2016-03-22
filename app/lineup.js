57254190999843052cd439b59817f8895517737e
Vanessa Virgitti <virgitti@gmail.com>
2016-03-22 15:25:00 +0000

    Feature select a pitch

--- app/lineup.js
+++ app/lineup.js
@@ -1,11 +1,20 @@
 'use strict';
 
+import _ from 'lodash';
+
 class Lineup {
 
   constructor() {
-
+    this.pitch = {};
   }
 
+  select (roster, playerPosition) {
+     return _.filter(roster, {positions: [{position: playerPosition}]})
+  }
 }
 
 export default Lineup;
+
+function highestRating (roster) {
+  _.orderBy(roster, ['rating'], ['desc'])
+}
--- test/unit/lineup.spec.js
+++ test/unit/lineup.spec.js
@@ -3,10 +3,11 @@
 import chai from 'chai';
 import {expect} from 'chai';
 import sinon from 'sinon';
+import _ from 'lodash';
 
 import Lineup from './../../app/lineup';
 
-describe.skip('Lineup', () => {
+describe.only('Lineup', () => {
 
   let sandbox;
 
@@ -19,35 +20,37 @@ describe.skip('Lineup', () => {
   });
 
   const roster = [
-    {firstName: 'Kat', positions: [{position: 'outfield', rating: 2}]},
+    {firstName: 'Kat', positions: [{position: 'outfield', rating: 1}]},
     {firstName: 'Dan', positions: [{position: 'outfield', rating: 2}]},
-    {firstName: 'Papi', positions: [{position: 'pitch', rating: 2}]},
-    {firstName: 'Mike', positions: [{position: '3rd base', rating: 2}]},
-    {firstName: 'Andy', positions: [{position: 'short stop', rating: 2}]},
-    {firstName: 'Dani', positions: [{position: '2nd base', rating: 2}]},
-    {firstName: 'Joelle', positions: [{position: 'outfield', rating: 2}]},
-    {firstName: 'Bec', positions: [{position: 'catch', rating: 2}]}
+    {firstName: 'Papi', positions: [{position: 'pitch', rating: 3}]},
+    {firstName: 'Mike', positions: [{position: '3rd base', rating: 4}]},
+    {firstName: 'Andy', positions: [{position: 'short stop', rating: 5}]},
+    {firstName: 'Dani', positions: [{position: '2nd base', rating: 6}]},
+    {firstName: 'Joelle', positions: [{position: 'outfield', rating: 7}]},
+    {firstName: 'Bec', positions: [{position: 'catch', rating: 8}]},
+    {firstName: 'Sven', positions: [{position: 'outfield', rating: 9}]}
   ];
 
-  it('can select players from the roster', () => {
-    const player = new Player ('Andy', 'Short Stop', 5);
+  const roster2 = [{firstname: 'Ken', positions: 'pitch'}];
 
-    lineup.select(player);
-    expect()
+  it('can select a player that is a pitch for the pitch position', () => {
+    const lineup = new Lineup();
+
+    expect(lineup.select(roster, 'pitch')).to.deep.equal([{firstName: 'Papi', positions: [{position: 'pitch', rating: 3}]}]);
+  });
+
+  it('can select the player with the highest rating for a specific position', () => {
+    const lineup = new Lineup();
+
+    const newPitch = {firstName: 'Tyrone', positions: [{position: 'pitch', rating: 4}]};
+    roster.push(newPitch);
+    console.log(lineup.select(roster, 'pitch'));
+    expect(lineup.select(roster, 'pitch')).to.deep.equal(newPitch);
+
   });
 
   it('knows that the minimum number of players is 10', () => {
     // use sinon to mock a roster and then return an error when then number is not 10
-
-    sandbox.spy(roster, 'retrieve');
-
-
-    // const roster = sinon.spy(Roster);
-    // expect(lineup.numberOfPlayers(8, players)).to.throw(Error, 'There are not enough players on the roster' );
-  });
-
-  it.skip('can select the best players per position', () => {
-    // use sinon mock
   });
 
 });
